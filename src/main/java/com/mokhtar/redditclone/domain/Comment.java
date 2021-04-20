df5bf65929a3d7cd7c38c31dd325e708593c9fb3
ahmedmokhtar97 <ahmad62380@student.must.edu.eg>
2021-04-20 22:58:36 +0200

    Added class Auditable and extend it in Link & comment

--- src/main/java/com/mokhtar/redditclone/domain/Comment.java
+++ src/main/java/com/mokhtar/redditclone/domain/Comment.java
@@ -6,15 +6,21 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable {
 
     @Id
     @GeneratedValue
     private Long id;
     private String body;
+
+
+    @ManyToOne
+    private Link link;
+
 
 }
--- src/main/java/com/mokhtar/redditclone/domain/Link.java
+++ src/main/java/com/mokhtar/redditclone/domain/Link.java
@@ -4,14 +4,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Link {
+public class Link extends Auditable {
 
     @Id
     @GeneratedValue
@@ -21,5 +21,8 @@ public class Link {
     @NonNull
     private String url;
 
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 }
