e1487f818aa8b85966f525b6a8304c97f97ddbb0
ahmedmokhtar97 <ahmedmokhtar46@gmail.com>
2021-04-20 13:07:17 +0200

    built our entities and refactored it using lombok

--- src/main/java/com/mokhtar/redditclone/RedditCloneApplication.java
+++ src/main/java/com/mokhtar/redditclone/RedditCloneApplication.java
@@ -1,13 +1,36 @@
 package com.mokhtar.redditclone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+
 public class RedditCloneApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedditCloneApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RedditCloneApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner runnerDev(){
+        return args -> {
+            logger.error("You are at Error level");
+            logger.warn("You are at Warn level");
+            logger.info("You are at Info level");
+            logger.debug("You are at Debug level");
+            logger.trace("You are at Trace level");
+        };
+    }
+
 
 }
